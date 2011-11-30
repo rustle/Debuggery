959527281fa1ee5298d33a195e99ff45478c59af
Doug Russell <doug@everythingsolution.com>
2011-11-29 19:34:04 -0600

    Initial Commit

---
+++ Debuggery/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  Debuggery
+//
+//  Created by  on 11/29/11.
+//  Copyright (c) 2011 Doug Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Debuggery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Debuggery
+//
+//  Created by  on 11/29/11.
+//  Copyright (c) 2011 Doug Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESAppDelegate class]));
+	}
+}
